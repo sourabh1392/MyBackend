3292c4c2477e80944ea2c3cec3b69fd868e20776
sourabh1392 <sourabhpatil1167@gmail.com>
2022-10-13 09:31:02 +0530

    Branch Push Demo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-const express = require('express');
-var bodyParser = require('body-parser');
-
-const route = require('./routes/route.js');
-
-const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/', route);
-
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
-});
+Hi It's been 1 month in the Functionup and I am feeling very Happy
