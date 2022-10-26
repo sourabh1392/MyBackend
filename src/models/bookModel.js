ce76b5ce9befb377a306a1a9e14c4483eb7c28da
sourabh1392 <sourabhpatil1167@gmail.com>
2022-10-26 23:31:31 +0530

    Assignment MongodbQueries

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,20 +1,47 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
+const bookSchema= new mongoose.Schema({
+    bookName:{
+        type:String,
+        required :true
+    },
+    price:{
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
+    year:{
+        type: String,
+        default: 2021
+    },
+    tags: [String],
+    authorName:String,
+    totalPages:Number,
+    stockAvailable: Boolean
+
+},{timestamps:true});
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+module.exports = mongoose.model('Book', bookSchema) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Validation:
 //require:true
