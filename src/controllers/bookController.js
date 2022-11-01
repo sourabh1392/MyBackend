2e9886725b8d43cd89bea90b6b496886be52edb5
sourabh1392 <sourabhpatil1167@gmail.com>
2022-11-01 17:42:10 +0530

    Middleware1 Assignment

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,79 +1,94 @@
 const { count } = require("console")
 const BookModel = require("../models/bookModel")
 
-const createBook= async function (req, res) {
-    let data= req.body
+// const createBook= async function (req, res) {
+//     let data= req.body
+//     let savedData= await BookModel.create(data)
+//     res.send({msg: savedData})
 
-    let savedData= await BookModel.create(data)
-    res.send({msg: savedData})
+
+// const getBooksData = async function (req, res) {
+//     let allBooks = await BookModel.find({ authorName: "" })
+//     console.log(allBooks)
+//     if (allBooks.length > 0) res.send({ msg: allBooks, condition: true })
+//     else res.send({ msg: "No books found", condition: false })
+// }
+
+
+// const updateBooks = async function (req, res) {
+//     let data = req.body // {sales: "1200"}
+//     // let allBooks= await BookModel.updateMany( 
+//     //     { author: "SK"} , //condition
+//     //     { $set: data } //update in data
+//     //  )
+//     let allBooks = await BookModel.findOneAndUpdate(
+//         { authorName: "ABC" }, //condition
+//         { $set: data }, //update in data
+//         { new: true, upsert: true } ,// new: true - will give you back the updated document // Upsert: it finds and updates the document but if the doc is not found(i.e it does not exist) then it creates a new document i.e UPdate Or inSERT
+//     )
+
+//     res.send({ msg: allBooks })
+// }
+
+// const deleteBooks = async function (req, res) {
+//     // let data = req.body 
+//     let allBooks = await BookModel.updateMany(
+//         { authorName: "FI" }, //condition
+//         { $set: { isDeleted: true } }, //update in data
+//         { new: true } ,
+//     )
+
+//     res.send({ msg: allBooks })
+// }
+
+
+
+// const totalSalesPerAuthor = async function (req, res) {
+//     // let data = req.body 
+//     let allAuthorSales = await BookModel.aggregate(
+//         [
+//             { $group: { _id: "$authorName", totalNumberOfSales: { $sum: "$sales" } } },
+//             { $sort: { totalNumberOfSales: -1 } }
+//         ]
+//     )
+
+//     res.send({ msg: allAuthorSales })
+// }
+
+
+
+// }
+// module.exports.deleteBooks = deleteBooks
+// module.exports.totalSalesPerAuthor = totalSalesPerAuthor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//<-------------Assignment ------------>
+
+
+const createBook=function(req,res){
+    res.send("My first Data")
 }
-
-
-
-
-
-
-const getBooksData = async function (req, res) {
-    let allBooks = await BookModel.find({ authorName: "HO" })
-    console.log(allBooks)
-    if (allBooks.length > 0) res.send({ msg: allBooks, condition: true })
-    else res.send({ msg: "No books found", condition: false })
+const getBook=function(req,res){
+    res.send("My Second  Data")
 }
-
-
-const updateBooks = async function (req, res) {
-    let data = req.body // {sales: "1200"}
-    // let allBooks= await BookModel.updateMany( 
-    //     { author: "SK"} , //condition
-    //     { $set: data } //update in data
-    //  )
-    let allBooks = await BookModel.findOneAndUpdate(
-        { authorName: "ABC" }, //condition
-        { $set: data }, //update in data
-        { new: true, upsert: true } ,// new: true - will give you back the updated document // Upsert: it finds and updates the document but if the doc is not found(i.e it does not exist) then it creates a new document i.e UPdate Or inSERT
-    )
-
-    res.send({ msg: allBooks })
+const updateBook=function(req,res){
+    res.send("My Third Data")
 }
-
-const deleteBooks = async function (req, res) {
-    // let data = req.body 
-    let allBooks = await BookModel.updateMany(
-        { authorName: "FI" }, //condition
-        { $set: { isDeleted: true } }, //update in data
-        { new: true } ,
-    )
-
-    res.send({ msg: allBooks })
-}
-
-
-
-const totalSalesPerAuthor = async function (req, res) {
-    // let data = req.body 
-    let allAuthorSales = await BookModel.aggregate(
-        [
-            { $group: { _id: "$authorName", totalNumberOfSales: { $sum: "$sales" } } },
-            { $sort: { totalNumberOfSales: -1 } }
-        ]
-    )
-
-    res.send({ msg: allAuthorSales })
-}
-
-
-
-
-// CRUD OPERATIONS:
-// CREATE
-// READ
-// UPDATE
-// DELETE
-
 
 
 module.exports.createBook = createBook
-module.exports.getBooksData = getBooksData
-module.exports.updateBooks = updateBooks
-module.exports.deleteBooks = deleteBooks
-module.exports.totalSalesPerAuthor = totalSalesPerAuthor
+module.exports.getBook = getBook
+module.exports.updateBook = updateBook
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const IP = require('ip')
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
@@ -8,18 +9,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://SourabhPatil1392:Patils1392@newproject.orvb1ly.mongodb.net/Sourabh", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
-app.use (
+
+
+
+
+//<-------Assignment------>
+const date1 = new Date
+app.use('/',
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+        console.log(date1, IP.address(), req.path)
+        next()
+    }
+)
+
+
+
+
 
 app.use('/', route);
 
