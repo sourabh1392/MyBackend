a172036d077915e9132fb0a5618d581f49ff4ca7
sourabh1392 <sourabhpatil1167@gmail.com>
2022-11-04 01:04:45 +0530

    assignment auth1

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,17 +2,15 @@ const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
 router.post("/users", userController.createUser  )
 
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+router.get("/getUserData/:userId", userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+router.put("/updateUser/:userId", userController.updateUser)
+
+router.delete("/deleteUser/:userId",userController.deleteUser)
 
 module.exports = router;
